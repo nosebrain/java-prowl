5899ccda46bae1eee3bc8e9b176fda9a2adab116
Daniel Zoller <nosebrain@gmx.net>
2012-05-12 15:50:40 +0200

    cleanup

--- src/main/java/de/nosebrain/prowl/NotificationPriority.java
+++ src/main/java/de/nosebrain/prowl/NotificationPriority.java
@@ -27,35 +27,35 @@ package de.nosebrain.prowl;
  * @author nosebrain
  */
 public enum NotificationPriority {
-	
+
 	/**
 	 * very low priority
 	 */
 	VERY_LOW(-2),
-	
+
 	/**
 	 * moderate priority
 	 */
 	MODERATE(-1),
-	
+
 	/**
 	 * normal priority
 	 */
 	NORMAL(0),
-	
+
 	/**
 	 * high priority
 	 */
 	HIGH(1),
-	
+
 	/**
 	 * emergency priority
 	 */
 	EMERGENCY(2);
-	
+
 	private final int value;
-	
-	private NotificationPriority(int value) {
+
+	private NotificationPriority(final int value) {
 		this.value = value;
 	}
 
@@ -63,6 +63,6 @@ public enum NotificationPriority {
 	 * @return the value
 	 */
 	public int getValue() {
-		return value;
+		return this.value;
 	}
 }
